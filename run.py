cf5ce3a81ff42bf2b8aae46225561300b0a5144f
dooco <padraigdooley@gmail.com>
2022-09-21 21:00:03 +0000

    import gspread  and google aut code

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
 # python code goes here
+import gspread
+from google.outh2.service_account import Credentials
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love-sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+        
+print(data)
