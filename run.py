06b2caf7c48d3679c8e8a4304048cf66554c04df
dooco <padraigdooley@gmail.com>
2022-09-22 17:54:25 +0000

    Resolve typo

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
-# python code goes here
 import gspread
-from google.outh2.service_account import Credentials
+from google.oauth2.service_account import Credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-CREDS = credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love-sandwiches')
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 
